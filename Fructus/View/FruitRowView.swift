bd97a3e6c43b6ae385d12311cf4ca5031c04ecbd
Prithvi Rao Ratti <prithvimagic@gmail.com>
2021-11-22 20:23:44 +0530

    Fruit Row View Added

---
+++ Fructus/View/FruitRowView.swift
@@ -0,0 +1,38 @@
+//
+//  FruitRowView.swift
+//  Fructus
+//
+//  Created by Ratti on 22/11/21.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    var fruit: Fruit
+    var body: some View {
+        HStack{
+            Image(fruit.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80, alignment: .center)
+                .background(LinearGradient(colors: fruit.gradientColors, startPoint: .top, endPoint: .bottom))
+                .cornerRadius(8)
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 3, x: 2, y: 2)
+            
+            VStack(alignment: .leading){
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundColor(Color.secondary)
+            }
+        }
+    }
+}
+
+struct FruitRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitRowView(fruit: arrFruit.first!).previewLayout(.sizeThatFits).padding()
+    }
+}
